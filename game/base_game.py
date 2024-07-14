6e411750d8f87d9fddbf78ed204819ebdf1b4e7c
Teodor Kostadinov <teodor.sv.kostadinov@gmail.com>
2024-07-14 21:08:32 +0300

    Add ship rotate move functionality

--- game/base_game.py
+++ game/base_game.py
@@ -1,4 +1,5 @@
 import random
+from collections import defaultdict
 import pygame
 
 from board import Board
@@ -10,24 +11,27 @@ class BaseGame():
     def __init__(self):
         self.board = Board()
         self.taken_coordinates = set()
-        self.ships = [
-            Ship(1),
-            Ship(1),
-            Ship(1),
-            Ship(1),
-            Ship(2),
-            Ship(2),
-            Ship(2),
-            Ship(3),
-            Ship(3),
-            Ship(4)
-        ]
+        self.ships_map = defaultdict(list)
+        self.unplaced_ships = self.get_base_game_ships()
+
+    def get_base_game_ships(self):
+        return {Ship(1),
+                Ship(1),
+                Ship(1),
+                Ship(1),
+                Ship(2),
+                Ship(2),
+                Ship(2),
+                Ship(3),
+                Ship(3),
+                Ship(4)
+                }
 
     def get_random_start_for_ship(self, ship_length, is_horizontal, board_border):
         return random.randint(0, board_border - 1 - is_horizontal * ship_length)
 
     def random_shuffle_ships(self):
-        for ship in self.ships:
+        for ship in sorted(list(self.unplaced_ships), key=lambda ship: -ship.ship_length):
             placed = False
 
             while not placed:
@@ -37,23 +41,55 @@ class BaseGame():
                 col = self.get_random_start_for_ship(
                     ship.ship_length, is_horizontal, self.board.BOARD_COLS)
 
-                ship.move_ship(row, col, is_horizontal)
+                ship.move(row, col, is_horizontal)
 
-                if ship.is_in_board(self.board) and not self.check_overlap(ship):
+                if self.is_ship_placement_valid(ship):
                     placed = True
-                    self.take_coordinates_from_placement(ship)
+                    self.place_ship(ship)
 
-    def take_coordinates_from_placement(self, ship):
+    def is_ship_placement_valid(self, ship):
+        return ship.is_in_board(self.board) and not self.check_overlap(ship)
+
+    def place_ship(self, ship):
+        self.ships_map[ship.row, ship.col].append(ship)
+        self.unplaced_ships.add(ship)
+        self.occupy_coordinates_from_placement(ship)
+
+    def remove_ship(self, ship):
+        self.ships_map[ship.row, ship.col].remove(ship)
+        self.unplaced_ships.remove(ship)
+        self.occupy_coordinates_from_placement(ship, True)
+
+    def move_ship(self, ship, new_row, new_col, new_is_horizontal):
+        old_row, old_col, old_is_horizontal = ship.row, ship.col, ship.is_horizontal
+        self.remove_ship(ship)
+
+        ship.move(new_row, new_col, new_is_horizontal)
+
+        if not self.is_ship_placement_valid(ship):
+            ship.move(old_row, old_col, old_is_horizontal)
+            self.place_ship(ship)
+            raise ValueError("Can not move ship")
+
+        self.place_ship(ship)
+
+    def flip_ship(self, ship):
+        self.move_ship(ship, ship.row, ship.col, not ship.is_horizontal)
+
+    def occupy_coordinates_from_placement(self, ship, reverse=False):
         adjacent_offsets = [
             (dx, dy)
-            for dx in (-1, 0, 1) # Change to 0 only to allow adjacency
+            for dx in (-1, 0, 1)  # Change to 0 only to allow adjacency
             for dy in (-1, 0, 1)
         ]
+
+        function_string = 'add' if not reverse else 'discard'
+        function = self.taken_coordinates.__getattribute__(function_string)
 
         for coord in ship.coordinates:
             for dx, dy in adjacent_offsets:
                 adj_coord = (coord[0] + dx, coord[1] + dy)
-                self.taken_coordinates.add(adj_coord)
+                function(adj_coord)
 
     def check_overlap(self, new_ship):
         return any(coord in self.taken_coordinates for coord in new_ship.coordinates)
@@ -61,14 +97,15 @@ class BaseGame():
     def __repr__(self):
 
         repr_str = ""
-        for ship in self.ships:
+        for ship in self.ships_map.values():
             repr_str += str(ship) + '\n'
 
         board_representation = [['-' for _ in range(10)] for _ in range(10)]
 
-        for ship in self.ships:
-            for row, col in ship.coordinates:
-                board_representation[row][col] = str(ship.ship_length)
+        for ship_list in self.ships_map.values():
+            for ship in ship_list:
+                for row, col in ship.coordinates:
+                    board_representation[row][col] = str(ship.ship_length)
 
         for row in board_representation:
             repr_str += ' '.join(row) + '\n'
@@ -81,3 +118,9 @@ if __name__ == "__main__":
     game = BaseGame()
     game.random_shuffle_ships()
     print(game)
+
+    for ship in list(game.ships_map.values())[0]:
+        print("Flipping ship", ship)
+        game.flip_ship(ship)
+        print(game)
+        break
