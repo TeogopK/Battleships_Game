54389b678e77b6e47513b17b2b57dd683ac4b78e
Teodor Kostadinov <teodor.sv.kostadinov@gmail.com>
2024-08-14 21:58:35 +0300

    Fix wait for opponent freezing the UI

--- game/menus/wait_for_opponent_menu.py
+++ game/menus/wait_for_opponent_menu.py
@@ -4,6 +4,8 @@ from game.visuals.utils.buttons import BasicButton, GoBackButton
 import game.menus as menus
 from game.players.player import Player
 
+CHECK_OPPONENT_EVENT = pygame.USEREVENT + 1
+
 
 class WaitForOpponentMenu(menus.Menu):
     def __init__(self, player):
@@ -15,18 +17,24 @@ class WaitForOpponentMenu(menus.Menu):
 
         self.next_menu = None
 
+        pygame.time.set_timer(CHECK_OPPONENT_EVENT, 2000)
+
     def handle_event(self, event):
         if self.exit_room_button.is_active():
             response = self.player.exit_room()
             if response.get('status') == 'success':
+                pygame.time.set_timer(
+                    CHECK_OPPONENT_EVENT, 0)
                 self.next_menu = menus.MultiplayerMenu(
                     self.player.network_client)
 
-        self.check_has_opponent_joined()
+        if event.type == CHECK_OPPONENT_EVENT:
+            self.check_has_opponent_joined()
 
     def check_has_opponent_joined(self):
         response = self.player.has_opponent_joined()
         if response.get('status') == 'success':
+            pygame.time.set_timer(CHECK_OPPONENT_EVENT, 0)
             self.next_menu = menus.ShipPlacementMenu(self.player)
 
     def draw(self, screen):
