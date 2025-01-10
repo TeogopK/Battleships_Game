0a12189826f765cbdf606b0597da047fdc974d16
Teodor Kostadinov <teodor.sv.kostadinov@gmail.com>
2025-01-10 02:09:21 +0200

    Add package as executable code

--- game/libs/config_loader.py
+++ game/libs/config_loader.py
@@ -1,15 +1,19 @@
-import yaml
 import os
+import sys
+import yaml
 
 _config = None
-
 
 def load_config(file_path=None):
     global _config
     if _config is None:
-        # Determine default path relative to the current file
         if file_path is None:
-            file_path = os.path.join(os.path.dirname(__file__), "../config.yaml")
+            if getattr(sys, 'frozen', False):
+                # If running as a bundled app
+                file_path = os.path.join(sys._MEIPASS, "config.yaml")
+            else:
+                # Normal execution (development mode)
+                file_path = os.path.join(os.path.dirname(__file__), "../config.yaml")
 
         # Open and load the config
         with open(file_path, "r") as file:
