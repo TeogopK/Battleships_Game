44e8e273c8d748d109704ebd16b2426f67022cb4
n0tn1w <somedpm@gmail.com>
2025-01-12 23:08:42 +0200

    DotNET api for collecting stats

---
+++ api/ShipDataManagement/Program.cs
@@ -0,0 +1,31 @@
+using ShipDataManagement.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Configuration.AddEnvironmentVariables();
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IFakeDbOperations, FakeDbOperations>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+//app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
